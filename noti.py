fdaa61b9c42e8a338bc7e690648c54f5e36b00fd
Muhammed Muaaz Dawood <momosuli8@gmail.com>
2025-03-17 20:12:40 +0200

    Update noti.py

--- noti.py
+++ noti.py
@@ -22,10 +22,9 @@ for product in products:
     
     discount_percentage = (product['original_price'] - product['discounted_price']) / product['original_price'] * 100
     
-    if discount_percentage >= 50:
-        print(f"{product['product_name']} is now {discount_percentage:.2f}% off! Check it out: {product['product_link']}")
+    if discount_percentage >= 70:
+        message = f"{product['product_name']} at {product['discounted_price']}$ ({discount_percentage:.2f}% off). {product['product_link']}"
         # Send a notification if the discount is greater than or equal to 70%
-        message = f"{product['product_name']} is now {discount_percentage:.2f}% off! Check it out: {product['product_link']}"
-        pb.push_note(f"Great Deal on {product['product_name']}", message)
+        pb.push_note(f"Deal: {product['product_name']}", message)
 
 print("Notifications sent successfully!")
